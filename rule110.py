b4b294b359be1969b4d869a73d923e1a9d4e9240
santamariagio <65074398+santamariagio@users.noreply.github.com>
2020-06-05 15:59:58 +0200

    Update rule110.py
    The rule_110 function has been debugged adding a second list. Doing so, the function does not mutate the same list upon which it evaluates the life-or-death condition of the cell instead, it evaluates an element on the first list and, eventually, changes it in the second list.
    Additionally, the rule_110 function in now applied on every following generation through the generator function.

--- rule110.py
+++ rule110.py
@@ -1,29 +1,42 @@
-def rule_110(gen, i):
+def rule_110(gen, next_gen, i):
     """
-    Apply the rule 110 cellular automaton to the given generation.
+    Applies the rule 110 cellular automaton to the given generation.
+
+    The rule_110 function takes two lists and a number `i`,
+    corresponding to the position in the two lists, and returns a
+    list.  The list `gen` is kept unmutated whereas the list
+    `next_gen` could be mutated if one of the conditions for a cell's
+    state-change dictated by rule 110 occurs.
     """
     right = i + 1
     if right < len(gen):
         if gen[i] is 1:
             if gen[i + 1] is 1 and gen[i - 1] is 1:
-                gen[i] = 0
+                next_gen[i] = 0
         else:
             if gen[i + 1] is 1:
-                gen[i] = 1
-        rule_110(gen, right)
-    return gen
+                next_gen[i] = 1
+        rule_110(gen, next_gen, right)
+    return next_gen
 
 
-n = input("Insert a number between 0 and 65536: ")    # The input number `n` is
+def generator(gens):
+    """
+    Applies the rule_110 function to the last list of the list and 
+    appends the return value.  
+
+    The generator function takes a list of lists and returns a list of 
+    lists.
+    """
+    if len(gens) < 20:
+        gens.append(rule_110(gens[-1], gens[-1].copy(), 1))
+        generator(gens)
+    return gens
+
+
+n = input("Insert a number between 0 and 65536: ")  # The input number `n` is
 # converted into the first generation, that is a list of 0s and 1s
 # corresponding to `n` binary notation in 16 bits.
 binary_n = format(int(n), '016b')
 generation_zero = [int(d) for d in binary_n]
-print(generation_zero)
-generation_to_process = generation_zero
-generation = 0
-while generation < 20:    # Number of generations before the halt.
-    new_generation = rule_110(generation_to_process, 1)
-    print(new_generation)
-    generation_to_process = new_generation
-    generation += 1
+set(map(print, generator([generation_zero])))
